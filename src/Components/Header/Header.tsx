4d6d042d0602f3d8a1f9e252ba7dd730a08f6a31
Pavnuto <74875159+pafnuto@users.noreply.github.com>
2022-11-02 13:53:15 +0300

    Add tailwind

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,24 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+interface IHeaderProps {
+    activeTab: string;
+    pathName: string;
+    handleActiveTab: boolean;
+  }
+
+  
+const Header = () => {
+    return (
+        switch (key) {
+            case value:
+                
+                break;
+        
+            default:
+                break;
+        }
+    );
+};
+
+export default Header;
